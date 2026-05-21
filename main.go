76d8ba4d685e49357d5ebf125ffad5a4d289d6df
kast <kast@kast.kast>
2026-05-21 21:14:16 +0300

    update login

--- main.go
+++ main.go
@@ -2,8 +2,13 @@ package main
 import (
 	"fmt"
 	"os"
-	"time"
+	//"encoding/json"
 )
+
+type session struct{
+	name string
+	access bool
+}
 
 
 func clearScreen() {
@@ -11,43 +16,59 @@ func clearScreen() {
     fmt.Print("\033[H") 
 }
 
-func start(){
+func start(cur_session *session){
 	fmt.Print("\033[?1049h")
 	fmt.Print("\033[H") 
 	fmt.Printf("PassStorage\nCreated by Kast\n")
-	fmt.Printf("Please input command (help - to see all commands)\n")
-	waitcommand()
+	waitcommand(cur_session)
 }
 
-func help(){
+func help(cur_session *session){
 	fmt.Printf("U can use this commands:\n")
 	fmt.Printf("'help' - Вывод данной справки\n")
 	fmt.Printf("'exit' - Выход из программы\n")
-	waitcommand()
+	waitcommand(cur_session)
 }
 
-func incorrect(){
+func incorrect(cur_session *session){
 	fmt.Printf("You input incorrect command ('help' to see list)\n")
-	waitcommand()
+	waitcommand(cur_session)
 }
 
-func waitcommand(){
+func login(cur_session *session){
+	if cur_session.name != ""{
+		clearScreen()
+		fmt.Printf("You allready login\n")
+		waitcommand(cur_session)
+	}
+	fmt.Printf("Hello, please input your username:\n")
+	var username string
+	fmt.Scanln(&username)
+}
+
+func waitcommand(cur_session *session){
+	fmt.Printf("Please input command (help - to see all commands)\n")
 	var command string
+	fmt.Printf("> ")
 	fmt.Scanln(&command)
 	if command == "help"{
 		clearScreen()
-		help()
+		help(cur_session)
 	}else if command == "exit"{
 		fmt.Print("\033[?1049l")
 		os.Exit(0)
+	}else if command == "login"{
+		login(cur_session)
+	}else if command == "new"{
+
 	}else{
 		clearScreen()
-		incorrect()
+		incorrect(cur_session)
 	}
 }
 
 func main(){
+	cur_session := new(session)
 	defer fmt.Print("\033[?1049l")
-	start()
-	time.Sleep(3 * time.Second)
+	start(cur_session)
 }
